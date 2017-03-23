3e5d41e492847e5218ca1caf2e44be9079f85323
Ken Takagi <ktakagi@Kens-MacBook-Pro.local>
2017-03-22 19:31:50 -0700

    Initial commit

---
+++ client/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+require('../styles/application.scss');
+
+ReactDOM.render(<App />, document.getElementById('react-root'));
