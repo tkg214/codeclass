8c53da8ca6dc7aa4e2e100633ccb7cab36c3eb11
sharon sw <sharonsuetwli@gmail.com>
2017-03-24 16:20:11 +0000

    Adds room_terminal.scss

--- client/src/actions/editor.js
+++ client/src/actions/editor.js
@@ -2,7 +2,7 @@ import { ActionTypes } from './constants/ActionTypes';
 
 export function updateEditorValues(val) {
   return {
-    type: ActionTypes.UPDATE_EDITOR_VALUES,
+    type: 'UPDATE_EDITOR_VALUES',
     payload: {
       value: val
     }
