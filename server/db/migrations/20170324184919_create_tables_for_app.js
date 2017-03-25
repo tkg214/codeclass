6d1ac10b7eddbf2ab6071c95262018cca401cf7f
Danton Nguyen <dantonnguyen88@gmail.com>
2017-03-25 11:30:23 -0700

    Populate first migration file

--- server/db/migrations/20170324184919_create_tables_for_app.js
+++ server/db/migrations/20170324184919_create_tables_for_app.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable("users"), (table) => {
+    knex.schema.createTable("users", (table) => {
       table.increments();
       table.string('github_login').notNullable;
       table.string('github_avatar').notNullable;
       table.string('github_name').notNullable;
-    }
+    }),
 
     knex.schema.createTable("classrooms", (table) => {
       table.increments();
@@ -14,14 +14,38 @@ exports.up = function(knex, Promise) {
       table.string('language_id').notNullable();
       table.boolean('editorLocked').notNullable();
       table.boolean('chatLocked').notNullable();
-      table.foreign('user_id').references('users.id')
+      table.integer('user_id').unsigned();
+      table.foreign('user_id').references('users.id');
     }),
 
+    knex.schema.createTable("edits", (table) => {
+      table.increments();
+      table.text('content').notNullable();
+      table.integer('classroom_id').unsigned();
+      table.foreign('classroom_id').references('classrooms.id');
+    }),
 
-  ])
+    knex.schema.createTable("messages", (table) => {
+      table.increments();
+      table.text('content').notNullable();
+      table.integer('user_id').unsigned();
+      table.foreign('user_id').references('users.id');
+      table.integer('classroom_id').unsigned();
+      table.foreign('classroom_id').references('classrooms.id');
+    })
+
+
+  ]);
 
 };
 
 exports.down = function(knex, Promise) {
+
+  return Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('classrooms'),
+    knex.schema.dropTable('edits'),
+    knex.schema.dropTable('messages'),
+  ])
 
 };
