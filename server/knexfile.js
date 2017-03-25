8324cc9fbf185b6ae99fc74799e5f1e7923bb271
Danton Nguyen <dantonnguyen88@gmail.com>
2017-03-24 18:42:38 -0700

    Initialize Knex

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,27 +3,23 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+  client: 'postgresql',
+  connection: {
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME,
+    port     : process.env.DB_PORT,
+    ssl      : process.env.DB_SSL
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    directory: './db/migrations',
+    tableName: 'migrations'
   },
+  seeds: {
+    directory: './db/seeds'
+  }
+},
 
   production: {
     client: 'postgresql',
