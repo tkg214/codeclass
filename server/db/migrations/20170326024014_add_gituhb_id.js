abe70e959a7131a3c0713e2371c5ff0beffab540
cwkosman <cwkosman@gmail.com>
2017-03-26 03:21:13 +0000

    Add access token field to migration

--- server/db/migrations/20170326024014_add_gituhb_id.js
+++ server/db/migrations/20170326024014_add_gituhb_id.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('users', t => {
       t.string('github_id').notNullable;
+      t.string('github_access_token').notNullable;
     })
   ]);
 };
@@ -11,6 +12,7 @@ exports.down = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('users', t => {
       t.dropColumn('github_id');
+      t.dropColumn('github_access_token');
     })
   ]);
 };
