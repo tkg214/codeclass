a2b8a73dc524569048ca06571d36d29ab4c52e59
sharon sw <sharonsuetwli@gmail.com>
2017-03-26 07:32:28 +0000

    Moves componentsDidUpdate to Terminal.jsx

--- client/src/components/RoomApp.jsx
+++ client/src/components/RoomApp.jsx
@@ -4,7 +4,7 @@ import EnvContainer from './env/EnvContainer.jsx';
 import ChatContainer from './chat/ChatContainer.jsx';
 
 class RoomApp extends Component {
-
+  
   render() {
     return (
       <div>
