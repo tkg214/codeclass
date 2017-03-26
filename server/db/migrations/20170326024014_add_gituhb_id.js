a04f037c3d6359d41cd0917a552e486c1d3cdcf9
cwkosman <cwkosman@gmail.com>
2017-03-26 03:07:25 +0000

    Migraton: add github_id to users table

--- server/db/migrations/20170326024014_add_gituhb_id.js
+++ server/db/migrations/20170326024014_add_gituhb_id.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.table('users', t => {
+      t.string('github_id').notNullable;
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.table('users', t => {
+      t.dropColumn('github_id');
+    })
+  ]);
 };
