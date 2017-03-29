2d5fd4f809c9f5010ec278db6c1a8d5133ecc9b1
Chuck Kosman <cwkosman@gmail.com>
2017-03-29 16:01:05 -0700

    Configure webpack.production.config.js

---
+++ client/webpack.production.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/client.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      options: { presets: ['es2015', 'react', 'stage-0'] }
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    })
+  ]
+};
