0eb5be9b96482299a49d13a6dd3d249dd93f5c41
Danton Nguyen <dantonnguyen88@gmail.com>
2017-03-31 11:46:11 -0700

    Add more css styling to user room cards

--- server/public/scripts/card_functions.js
+++ server/public/scripts/card_functions.js
@@ -1,1 +1,13 @@
 //ideally this will be used to hold the axios code for delete/removing classrooms
+
+
+$(document).ready(function(){
+
+  $('.delete-card-icon').click( () => {
+    console.log(this);
+  })
+
+
+
+
+});
