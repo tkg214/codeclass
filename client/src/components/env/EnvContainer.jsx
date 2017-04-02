8181b58f410fe318dc97a97a079047fcaa2d30ad
Sharon <sharonsuetwli@gmail.com>
2017-04-02 15:15:30 -0700

    Fixes disappearing editor bug

--- client/src/components/env/EnvContainer.jsx
+++ client/src/components/env/EnvContainer.jsx
@@ -15,12 +15,12 @@ class EnvContainer extends Component {
     return (
       <div className='env-container'>
         <div className='env-nav-container'>
-          <div className='row1'>
+          <div className='row'>
             <EnvHeader
               roomTitle={roomControls.roomTitle}
               language={roomControls.language}/>
           </div>
-          <div className='row1'>
+          <div className='row'>
             <GistContainer
               actions={actions}
               saveStatus={gist.saveStatus}
