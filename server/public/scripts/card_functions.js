fc01a37fe1277eecd54f625a6060c0d97ab10f55
Danton Nguyen <dantonnguyen88@gmail.com>
2017-04-02 17:32:13 -0700

    Use Isotope to sort cards

--- server/public/scripts/card_functions.js
+++ server/public/scripts/card_functions.js
@@ -1,13 +1,38 @@
 //ideally this will be used to hold the axios code for delete/removing classrooms
 
+$(document).ready(function() {
 
-$(document).ready(function(){
-
-  $('.delete-card-icon').click( () => {
-    console.log(this);
-  })
-
+  // $('.delete-card-icon').click(() => {
+  //   console.log(this);
+  // })
 
 
+  const $grid = $('.myrooms-container').isotope({
+    itemSelector: '.card',
+    layoutMode: 'fitRows',
+    getSortData: {
+      topic: '.card-header',
+      language: '[data-language]'
+    }
+  });
+
+  $('.btn-group.filter-language-group').on('click', 'button', function() {
+    const filterValue = $(this).attr('data-filter');
+    console.log('TRIGGERED');
+    $grid.isotope({filter: filterValue});
+  });
+
+  $('.sort-by-button-group').on('click', 'button', function() {
+    const sortByValue = $(this).attr('data-sort-by');
+    const sortOrder = $(this).attr('data-order');
+    if (sortOrder === "ascending") {
+
+      $grid.isotope({sortBy: sortByValue, sortAscending: true});
+
+    } else {
+      console.log('descending');
+      $grid.isotope({sortBy: sortByValue, sortAscending: false});
+    }
+  });
 
 });
