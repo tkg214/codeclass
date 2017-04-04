e29d042e0f4546b08d2f8239784309f05a9ea070
Danton Nguyen <dantonnguyen88@gmail.com>
2017-04-04 01:50:50 -0700

    add modal to display on show_users page

--- server/public/scripts/card_functions.js
+++ server/public/scripts/card_functions.js
@@ -32,4 +32,32 @@ $(document).ready(function() {
     }
   });
 
+  // get the elements
+  const modal = document.getElementById('myModal')
+  const button = document.getElementById("modal-button")
+  const close = document.querySelectorAll(".closeModal")[0]
+  const clickable = document.querySelectorAll('.clickable')
+
+  const openModal = function() {
+  	modal.style.display = "block";
+    $('body').addClass('modal-active');
+
+  }
+  const closeModal = function() {
+  		modal.style.display = "none";
+      $('body').removeClass('modal-active');
+  	}
+  	//event listeners
+  button.addEventListener('click', openModal, false)
+  close.addEventListener('click', closeModal, false)
+  for (let i = 0; i < clickable.length; i++) {
+  	clickable[i].addEventListener('click', openModal, false)
+  }
+
+  window.onclick = function(event) {
+  	if (event.target == modal) {
+  		modal.style.display = "none"
+  	}
+  }
+
 });
