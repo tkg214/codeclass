b2802345b6023df952f4c7ed4fa21677a4a9a740
Chuck Kosman <cwkosman@gmail.com>
2017-04-05 10:42:26 -0700

    Revert to conditional bundleSrc

--- client/src/helpers/resize.js
+++ client/src/helpers/resize.js
@@ -8,15 +8,20 @@ $(function() {
   }
 
   function setTerminalWidth(){
-    const totalWidth = $(window).width();
-    const chatWidth = $('.chat-container').width();
-    const chatBarWith = $('.chat-notificaiton-bar').width();
-    const terminalWidth = $((totalWith - chatWidth - chatBarWidth) / totalWidth) * 100 - 0.01;
-    $('.terminal').css('width', terminalWidth + 'vw');
+    const editorWidth = $('#brace-editor').width();
+    // const totalWidth = $(window).width();
+    // console.log('total width: ', totalWidth)
+    // const chatWidth = $('.chat-container').width();
+    // console.log('chat width: ', chatWidth);
+    // const chatBarWidth = $('.chat-notification-bar').width();
+    // console.log('chatbar width: ', chatBarWidth);
+    // const terminalWidth = ((totalWidth - chatWidth - chatBarWidth) / totalWidth) * 100 - 0.01;
+    // console.log(terminalWidth);
+    $('.terminal').css(width, editorWidth + 'px');
   }
 
   $(document).ready(setTerminalWidth);
   $(window).resize(setTerminalWidth);
-  $(document).ready(setHeight);
-  $(window).resize(setHeight);
+  $(document).ready(setHeight());
+  $(window).resize(setHeight());
 });
